15144b6efcb58c9049da4c01cddbe83f994d081d
Roby Tanama <54106843+tanamaroby@users.noreply.github.com>
2025-05-22 18:53:54 +0700

    feat: made plantation card collapsible

--- src/components/plantations/PlantationCard.tsx
+++ src/components/plantations/PlantationCard.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { capitalize } from "lodash";
-import { Search } from "lucide-react";
+import { ChevronsUpDown, Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
@@ -14,6 +15,11 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
 import { Label } from "../ui/label";
 import PlantationDeleteButton from "./PlantationDeleteButton";
 
@@ -36,6 +42,7 @@ const PlantationItem: React.FC<{
 };
 
 const PlantationCard: React.FC<PlantationCardProps> = (props) => {
+  const [open, setOpen] = React.useState(false);
   return (
     <Card className="hover:shadow-lg transition-shadow pt-0 overflow-hidden">
       <Image
@@ -51,57 +58,77 @@ const PlantationCard: React.FC<PlantationCardProps> = (props) => {
         height={344}
         className="h-[200px] w-auto object-cover"
       />
-      <CardHeader className="pb-3">
-        <CardTitle className="text-lg truncate">{props.name}</CardTitle>
-        <CardDescription className="text-xs">
-          Code: <span className="font-semibold">{props.code}</span> • Luas:{" "}
-          <span className="font-semibold">{props.areaTotalHa} Ha</span>
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-y-6 flex-1">
-        <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4">
-          <PlantationItem
-            label="Lokasi"
-            value={props.location}
-            className="sm:col-span-2"
-          />
-          <PlantationItem
-            label="Pemilik Kebun"
-            value={props.ownerCompany}
-            className="sm:col-span-2"
-          />
-          <PlantationItem label="Manager Kebun" value={props.managerName} />
-          <PlantationItem label="Nomor HP" value={props.contactNumber} />
-        </div>
+      <Collapsible open={open} onOpenChange={setOpen} className="flex-1">
+        <CardHeader className="pb-3">
+          <div className="flex items-center gap-2 justify-between">
+            <CardTitle className="text-lg truncate">{props.name}</CardTitle>
+            <CollapsibleTrigger asChild>
+              <Button
+                variant="ghost"
+                size="sm"
+                className="flex items-center gap-1 cursor-pointer"
+              >
+                <p className="text-sm">Expand</p>
+                <ChevronsUpDown className="h-4 w-4" />
+              </Button>
+            </CollapsibleTrigger>
+          </div>
 
-        <hr className="border-muted" />
+          <CardDescription className="text-xs">
+            Code: <span className="font-semibold">{props.code}</span> • Luas:{" "}
+            <span className="font-semibold">{props.areaTotalHa} Ha</span>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1">
+          <CollapsibleContent className="grid gap-y-6">
+            <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4">
+              <PlantationItem
+                label="Lokasi"
+                value={props.location}
+                className="sm:col-span-2"
+              />
+              <PlantationItem
+                label="Pemilik Kebun"
+                value={props.ownerCompany}
+                className="sm:col-span-2"
+              />
+              <PlantationItem label="Manager Kebun" value={props.managerName} />
+              <PlantationItem label="Nomor HP" value={props.contactNumber} />
+            </div>
 
-        <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4">
-          <PlantationItem label="Latitude" value={props.latitude?.toString()} />
-          <PlantationItem
-            label="Longitude"
-            value={props.longitude?.toString()}
-          />
-          <PlantationItem
-            label="Keterangan"
-            value={props.notes}
-            className="sm:col-span-2"
-          />
-        </div>
+            <hr className="border-muted" />
 
-        <hr className="border-muted" />
+            <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4">
+              <PlantationItem
+                label="Latitude"
+                value={props.latitude?.toString()}
+              />
+              <PlantationItem
+                label="Longitude"
+                value={props.longitude?.toString()}
+              />
+              <PlantationItem
+                label="Keterangan"
+                value={props.notes}
+                className="sm:col-span-2 line-clamp-2"
+              />
+            </div>
 
-        <div className="grid sm:grid-cols-2 gap-x-6 gap-y-2 text-xs text-muted-foreground">
-          <PlantationItem
-            label="Tanggal Dibuat"
-            value={props.createdAt.toDateString()}
-          />
-          <PlantationItem
-            label="Tanggal Update"
-            value={props.updatedAt.toDateString()}
-          />
-        </div>
-      </CardContent>
+            <hr className="border-muted" />
+
+            <div className="grid sm:grid-cols-2 gap-x-6 gap-y-2 text-xs text-muted-foreground">
+              <PlantationItem
+                label="Tanggal Dibuat"
+                value={props.createdAt.toDateString()}
+              />
+              <PlantationItem
+                label="Tanggal Update"
+                value={props.updatedAt.toDateString()}
+              />
+            </div>
+          </CollapsibleContent>
+        </CardContent>
+      </Collapsible>
       <CardFooter className="flex items-center justify-between border-t gap-4">
         <Link href={`/plantations/${props.id}`} className="col-span-3 w-full">
           <Button className="flex w-full items-center gap-2">
